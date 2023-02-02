9a63d18295561569a6d58bd9f3da18c01e37ad89
Ramesh.Mallina <mallinarc@hotmail.com>
2023-02-02 06:21:33 -0500

    builider pattern

---
+++ src/com/dp/builderpattern/Room.java
@@ -0,0 +1,11 @@
+package com.dp.builderpattern;
+
+public class Room {
+	private int length;
+	private int width;
+
+	public Room(final int length, final int width) {
+		this.length = length;
+		this.width = width;
+	}
+}
